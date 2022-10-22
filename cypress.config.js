fd323105f81772bdd6ef5de6047d11d6ef403209
Kevin Liang <lianglikun@deepbaysz.com>
2022-10-22 16:43:49 +0800

    report

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,13 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/report',
+    charts: true,
+    reportPageTitle: 'custom-title'
+  }
 });
